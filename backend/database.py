4a333796d09bdf899aef951ac67bc112ebeb9f04
Guadalupec-g <84210876+Guadalupec-g@users.noreply.github.com>
2025-07-16 22:20:05 -0600

    Corrijo configuración en database.py

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # ⚠️ Reemplazá con tus propios datos si cambian
-DATABASE_URL = "mysql+mysqlconnector://root:LupeSecure_2025@localhost:3306/infomundi"
+DATABASE_URL = "mysql+mysqlconnector://root:password@localhost:3306/infomundi"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
